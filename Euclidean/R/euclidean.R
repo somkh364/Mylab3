022e4d03382482ae1ee8079a0d79c3e3fc867a46
Somaya Rezai <somayarezai@Somayas-MacBook-Pro.local>
2017-09-17 17:41:02 +0200

    the new packaje created by skeleton

---
+++ Euclidean/R/euclidean.R
@@ -0,0 +1,14 @@
+euclidean <-
+function(a,b) {
+  
+  if (a> b){ smaller = b
+  } else {
+    smaller = a
+  }
+  for (i in 1:smaller){
+    if((a%%i == 0) && (b%%i == 0)){
+      GCD = i
+    }
+  }
+  return (GCD)
+}
