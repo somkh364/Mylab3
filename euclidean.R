de48e3d7fd8488e5e3e36a7eff6b99468f946222
Somaya Rezai <somayarezai@Somayas-MacBook-Pro.local>
2017-09-17 18:34:07 +0200

    Travis added

--- euclidean.R
+++ euclidean.R
@@ -1,3 +1,12 @@
+
+#' compute the greatest common divisor
+#' an efficient method for computing the greatest common divisor (GCD)
+#' @param a and b are numeric values which a > b
+#' @return GCD
+#' @export
+#' @references https://en.wikipedia.org/wiki/Euclidean_algorithm
+#' 
+
 euclidean <- function(a,b) {
   
   if (a> b){ smaller = b
